e92bd3283f3cb6e1d493ef3b5ab01a9d0b63a6a7
sandaleebandara <sandalee.bandara@gmail.com>
2022-05-19 22:23:39 +0530

    Array list operations

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.remove(0);
+        System.out.println(list.size());
+        System.out.println(list);
     }
 }
